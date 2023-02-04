9dbe304d57c0fe6ce05cea8b46727cfc91869fdf
LuanSilva7 <luan.vieiradasilva@hotmail.com>
2023-02-04 02:08:54 -0300

    Trabalho de Array

---
+++ script.js
@@ -0,0 +1,45 @@
+let nomes = [
+  "Alanzoka",
+  "Cellbit",
+  "Goulart",
+  "Os rapazes",
+  "LubaTV"
+]
+console.log(nomes)
+
+nomes.unshift("Morimura");
+console.log(nomes)
+
+nomes.pop()
+console.log(nomes)
+
+nomes.shift()
+console.log(nomes)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort();
+console.log(numbers)
+
+let obj = {
+name: "Luan",
+age: 23,
+pcgamer:0
+}
+console.log(obj)
+
+// 08 Adicione uma nova propriedade sem alterar seu objeto inicial.
+
+// 09 Remova uma propriedade desse objeto.
+
+// 10-Mostre no console todas as propriedades desse objeto.
+// (não consegui)
+
+let cadastro = [
+  {
+  nome:"Ana",
+  idade:23,
+  amigos: "Julia"}
+  // (não consigo botar mais de 1 pq da erro(ou eu que to fazendo errado msm))
+]
+console.log(cadastro)
+// não consegui fazer o resto
